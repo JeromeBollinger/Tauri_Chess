60b628ee2006195680dcad6fbc0f3c6aadc4666f
JeromeBollinger <jerome.f.bollinger@gmail.com>
2024-03-31 01:02:42 +0100

    chore: Add movement options of figures

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -13,3 +13,103 @@ fn main() {
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
+
+enum FigureType {
+    Pawn,
+    King,
+    Queen,
+    Bishop,
+    Knight,
+    Rook,
+}
+
+impl Figure {
+    fn movable(&self) -> Vec<Position> {
+        match &self.kind {
+            FigureType::Pawn => {
+                if self.color.white {
+                    vec![Position::new(self.position.x, self.position.y + 1)]
+                } else {
+                    vec![Position::new(self.position.x, self.position.y + 1)]
+                }
+            }
+            FigureType::King => vec![
+                Position::new(self.position.x - 1, self.position.y),
+                Position::new(self.position.x - 1, self.position.y + 1),
+                Position::new(self.position.x, self.position.y + 1),
+                Position::new(self.position.x + 1, self.position.y + 1),
+                Position::new(self.position.x + 1, self.position.y),
+                Position::new(self.position.x + 1, self.position.y - 1),
+                Position::new(self.position.x, self.position.y - 1),
+                Position::new(self.position.x - 1, self.position.y - 1),
+            ],
+            FigureType::Knight => vec![
+                Position::new(self.position.x - 2, self.position.y + 1),
+                Position::new(self.position.x - 1, self.position.y + 2),
+                Position::new(self.position.x + 1, self.position.y + 2),
+                Position::new(self.position.x + 2, self.position.y + 1),
+                Position::new(self.position.x + 1, self.position.y - 2),
+                Position::new(self.position.x + 2, self.position.y - 1),
+                Position::new(self.position.x - 2, self.position.y - 1),
+                Position::new(self.position.x - 1, self.position.y - 2),
+            ],
+            FigureType::Rook => {
+                let mut pos: Vec<Position> = vec![];
+                for distance in 1..7 {
+                    pos.push(Position::new(self.position.x + distance, self.position.y));
+                    pos.push(Position::new(self.position.x - distance, self.position.y));
+                    pos.push(Position::new(self.position.x, self.position.y + distance));
+                    pos.push(Position::new(self.position.x, self.position.y - distance));
+                }
+                pos
+            },
+            FigureType::Bishop => {
+                let mut pos: Vec<Position> = vec![];
+                for distance in 1..7 {
+                    pos.push(Position::new(self.position.x + distance, self.position.y + distance));
+                    pos.push(Position::new(self.position.x - distance, self.position.y - distance));
+                    pos.push(Position::new(self.position.x - distance, self.position.y + distance));
+                    pos.push(Position::new(self.position.x + distance, self.position.y - distance));
+                }
+                pos
+            },
+            FigureType::Queen => {
+                let mut pos: Vec<Position> = vec![];
+                for distance in 1..7 {
+                    pos.push(Position::new(self.position.x + distance, self.position.y + distance));
+                    pos.push(Position::new(self.position.x - distance, self.position.y - distance));
+                    pos.push(Position::new(self.position.x - distance, self.position.y + distance));
+                    pos.push(Position::new(self.position.x + distance, self.position.y - distance));
+                    pos.push(Position::new(self.position.x + distance, self.position.y));
+                    pos.push(Position::new(self.position.x - distance, self.position.y));
+                    pos.push(Position::new(self.position.x, self.position.y + distance));
+                    pos.push(Position::new(self.position.x, self.position.y - distance));
+                }
+                pos
+            },
+        }
+    }
+}
+
+struct Figure {
+    kind: FigureType,
+    position: Position,
+    color: Color,
+    alive: bool,
+}
+
+struct Position {
+    x: i32,
+    y: i32,
+}
+
+impl Position {
+    fn new(x: i32, y: i32) -> Position {
+        Position { x, y }
+    }
+}
+
+struct Color {
+    white: bool,
+}
+
