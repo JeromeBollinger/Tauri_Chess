5e4bac0325c48ddfbb55309c348908eea574bddf
JeromeBollinger <jerome.f.bollinger@gmail.com>
2024-04-01 22:32:21 +0200

    format file

--- src/chess_board.js
+++ src/chess_board.js
@@ -1,18 +1,18 @@
 const { invoke } = window.__TAURI__.tauri;
 const canva = document.getElementById("board");
 
-var canvas_length = window.screen.height/2;
-var rect_length = canvas_length/8;
+var canvas_length = window.screen.height / 2;
+var rect_length = canvas_length / 8;
 
 
-function fill_board(){
+function fillBoard() {
     canva.width = canvas_length;
     canva.height = canvas_length;
     let canvas = canva.getContext("2d");
     let n = 0;
     for (let x = 0; x <= 8; x++) {
         for (let y = 0; y <= 8; y++) {
-            if (n%2 == 0){
+            if (n % 2 == 0) {
                 canvas.fillStyle = "black"
             } else {
                 canvas.fillStyle = "white"
@@ -33,13 +33,13 @@ let figureShapes = [];
 window.addEventListener("load", () => {
     getBoard().then(
         board => {
-            figureShapes = drawFigures(board);
-        }).catch( error =>
+            redrawBoard(board)
+        }).catch(error =>
             console.log(error, "could not fetch board!!! ")
-    )
+        )
 });
 
-async function getOptions(figureId){
+async function getOptions(figureId) {
     let options = await invoke("get_options", { "figureId": figureId });
     return options;
 }
@@ -57,7 +57,7 @@ canva.addEventListener('click', e => {
         options => {
             var optionShapes = drawOptions(options)
         }
-    ).catch( error =>
+    ).catch(error =>
         console.log(error, "could not fetch options")
     );
 })
@@ -77,10 +77,10 @@ function drawFigure(figure) {
     canvas.fillStyle = "black"
     canvas.font = "12px serif";
     canvas.fillText(figure.kind, figure.position.x * rect_length + rect_length / 4, figure.position.y * rect_length + rect_length / 2 + 5);
-    return {"shape": circle, "object": figure};
+    return { "shape": circle, "object": figure };
 }
 
-function drawOptions(options){
+function drawOptions(options) {
     var optionShapes = [];
     options.forEach((option) => {
         optionShapes.push(drawOption(option));
@@ -94,7 +94,8 @@ function drawOption(option) {
     let circle = new Path2D();
     circle.arc(option.x * rect_length + rect_length / 2, option.y * rect_length + rect_length / 2, rect_length / 5, 0, 2 * Math.PI);
     canvas.fill(circle);
-    return {"shape": circle, "object": option};
+    return { "shape": circle, "object": option };
+}
 
 function clearBoard() {
     canva.getContext("2d").clearRect(0, 0, canva.width, canva.height);
